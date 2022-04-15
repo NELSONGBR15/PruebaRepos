c59a2d9d198664d348ffb67902ae5db43a8f8354
NelsonBR <nelson.bocanegra@campusvirtual.aunarvillavicencio.edu.co>
2022-04-14 21:34:19 -0500

    Creando componentes heredados

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../src/App.css';
+import tasks from './sample/tasks.json';
+import Tasks from './components/Tasks';
 
-/*
-function Helloworld(props){
-  console.log(props);
-  return(
-    <div id='hello'>
-      <h3>{props.subtitle}</h3>
-      {props.mytext}
-    </div>
-  );
-}*/
+console.log(tasks);
 
-class Helloworld extends React.Component{
+class App extends Component{
 
   state = {
-    show: true
+    tasks: tasks
   }
 
-toggleShow = () =>{
-  this.setState({show: !this.state.show})
-}
-
-render(){
-  if(this.state.show){
-    return(
-      <div id='hello'>
-      <h3>{this.props.subtitle}</h3>
-      {this.props.mytext}     
-      <button onClick={this.toggleShow} >Cambiar estado</button>
+  render(){
+    return <div>
+      <Tasks tasks={this.state.tasks}/>
     </div>
-      )    
-    }else{
-      return <h1>
-        No hay elementos
-        <button onClick={this.toggleShow}>
-          toggleShow
-          </button>
-        </h1> 
-    }
   }
 }
 
-//Llamar componentes
-function App() {
-  return (
-  <div>
-    Estos son mis componentes: 
-    <Helloworld mytext="Hello Fazt" subtitle = "lorem ispum"/>  
-    <Helloworld mytext="Hola mundo" subtitle="Component 2"/> 
-    <Helloworld mytext="Hola nelson" subtitle = "componente 3"/>  
-    </div>
-  );
-}
 
 export default App;

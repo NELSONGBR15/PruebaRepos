b97fa09abf119ac50d94e978fd0cd0d662e5d3a5
NelsonBR <nelson.bocanegra@campusvirtual.aunarvillavicencio.edu.co>
2022-04-14 21:56:08 -0500

    Estilos utilizando css puro

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,6 @@
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
 import React, {Component} from "react";
+import './Task.css';
 
 class Task extends Component{
     render(){
@@ -6,19 +8,29 @@ class Task extends Component{
         const { task } = this.props;
 
 
-        return <div>
+        return <p className="red">
             {task.title} -
             {task.description} -
             {task.done} -
             {task.id}
             <input type="checkbox" />
-            <button>
+            <button style = { btnDelete }>
                 x
             </button>       
-        </div>
+        </p>
 
 
     }
+}
+
+const btnDelete = {
+    fontSize: '24px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer'
 }
 
 export default Task;

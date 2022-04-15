da38f17d0fddf6648bf9303c37757d39387b5508
NelsonBR <nelson.bocanegra@campusvirtual.aunarvillavicencio.edu.co>
2022-04-14 19:19:04 -0500

    Prueba commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../src/App.css';
 
+/*
+function Helloworld(props){
+  console.log(props);
+  return(
+    <div id='hello'>
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
+  );
+}*/
+
+class Helloworld extends React.Component{
+
+  state = {
+    show: true
+  }
+
+toggleShow = () =>{
+  this.setState({show: !this.state.show})
+}
+
+render(){
+  if(this.state.show){
+    return(
+      <div id='hello'>
+      <h3>{this.props.subtitle}</h3>
+      {this.props.mytext}     
+      <button onClick={this.toggleShow} >Cambiar estado</button>
+    </div>
+      )    
+    }else{
+      return <h1>
+        No hay elementos
+        <button onClick={this.toggleShow}>
+          toggleShow
+          </button>
+        </h1> 
+    }
+  }
+}
+
+//Llamar componentes
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    Estos son mis componentes: 
+    <Helloworld mytext="Hello Fazt" subtitle = "lorem ispum"/>  
+    <Helloworld mytext="Hola mundo" subtitle="Component 2"/> 
+    <Helloworld mytext="Hola nelson" subtitle = "componente 3"/>  
     </div>
   );
 }
